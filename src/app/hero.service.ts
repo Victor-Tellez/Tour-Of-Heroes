b1a19109726b6c22035e6e4401ea63c24bb16ef2
VictorTellez219 <vyctortellez@gmail.com>
2020-03-23 23:54:19 -0400

    An asynchronous approach using subscribe to getting the heroes

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,8 +10,10 @@ export class HeroService {
 
   constructor() { }
 
-  getHeroes(): Hero[]{
-    return HEROES;
+  getHeroes(): Observable<Hero[]>{
+    // This returns an Observable<Hero[]> that emits a single value, 
+    // the array of mock heroes
+    return of(HEROES);
   }
 
 }
