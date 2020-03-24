388774442e87913fdee2b6c177e97d916a78cf5d
VictorTellez219 <vyctortellez@gmail.com>
2020-03-24 01:26:14 -0400

    Display message from heroservice & Bind to the MessageService

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -2,16 +2,19 @@ import { Injectable } from '@angular/core';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor() { }
+  constructor(private MessageService: MessageService) { }
 
   getHeroes(): Observable<Hero[]>{
-    // This returns an Observable<Hero[]> that emits a single value, 
+    // TODO: send the message _after_ fetching the heroes
+    this.messageService.add('HeroService: fetched heroes');
+    // This returns an Observable<Hero[]> that emits a single value,
     // the array of mock heroes
     return of(HEROES);
   }
